356dd7092b1686c4231f7a665268116eef0f8203
f-sander <fsander@mailbox.tu-berlin.de>
2015-01-17 18:36:36 +0100

    prototype project structure

---
+++ src/main/java/de/tuberlin/dima/aim3/SVD.java
@@ -0,0 +1,16 @@
+package de.tuberlin.dima.aim3;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+public class SVD {
+
+	public static void main(String[] args) throws Exception {
+
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		
+		// TODO: insert Lanczos data-flow here
+		
+		env.execute();
+
+	}
+}
