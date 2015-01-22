58e13517a2c637f936c4b03d6ce81b01a7e2df69
Nik Hille <nik.hille@macio.de>
2015-01-22 15:37:28 +0100

    Initial test skeleton

--- src/de/tuberlin/dima/aim3/SVD.java
+++ src/de/tuberlin/dima/aim3/SVD.java
@@ -1,16 +1,46 @@
 package de.tuberlin.dima.aim3;
 
+import de.tuberlin.dima.aim3.algorithms.Lanczos;
+import de.tuberlin.dima.aim3.datatypes.LanczosResult;
+import de.tuberlin.dima.aim3.operators.MatrixReader;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.mahout.math.MatrixWritable;
 
 public class SVD {
 
-  public static void main(String[] args) throws Exception {
+  private static final long LANCZOS_ITERATIONS = 500;
 
+  public static void main(String[] args) throws Exception {
     ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-    // TODO: insert Lanczos data-flow here
+    // Read the text file containing the input matrix line by line.
+    DataSource<String> inputMatrix = env.readTextFile("path/to/input/matrix");
 
+    // Parse each line of the input file to a tuple of (rowIndex, list(rowValues)).
+    DataSet<Tuple2<Long, Double[]>> matrix = inputMatrix.map(new MatrixReader());
+
+    // TODO: Using a reduce operator on the ungrouped data set, convert all (rowIndex, list(rowValues)) tuples into a
+    // single matrix instance. Implement custom Matrix class for this?
+    MatrixWritable A = new MatrixWritable();
+
+    // Apply the Lanczos algorithm to our input matrix and collect the result.
+    LanczosResult lanczosResult = Lanczos.process(A, LANCZOS_ITERATIONS);
+
+    // Extract the tridiagonal matrix and the matrix containing all Lanczos vectors from the result.
+    MatrixWritable Tmm = lanczosResult.getTmm();
+    MatrixWritable Vm  = lanczosResult.getVm();
+
+    // TODO: Apply the QR algorithm (?) to Tmm and collect the resulting eigenvalues and eigenvectors of Tmm.
+    // TODO: Omit all of Tmm's eigenvalues that were calculated as a function of Vm's non-orthogonal Lanczos vectors.
+    // TODO: Calculate A's eigenvectors by multiplying each eigenvector of Tmm with Vm.
+
+    // --> As Tmm's eigenvalues are approximately the same as A's eigenvalues, we now have A's eigenvalues as well as
+    //     A's eigenvectors.
+
+    // Execute the whole thing!
     env.execute();
-
   }
 }
