6aa1360a29a13baf70b84dd16a0efacb1beb17bb
Nik Hille <nik.hille@macio.de>
2015-01-22 17:11:50 +0100

    Fixed types

--- src/de/tuberlin/dima/aim3/algorithms/Lanczos.java
+++ src/de/tuberlin/dima/aim3/algorithms/Lanczos.java
@@ -1,9 +1,14 @@
 package de.tuberlin.dima.aim3.algorithms;
 
 import de.tuberlin.dima.aim3.datatypes.LanczosResult;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.MatrixWritable;
 import org.apache.mahout.math.Vector;
+
+import java.util.ArrayList;
 
 public final class Lanczos {
 
@@ -11,18 +16,18 @@ public final class Lanczos {
     // Private constructor pretty much makes this class static.
   }
 
-  public static LanczosResult process(MatrixWritable A, int m) {
+  public static LanczosResult process(DataSet<Tuple2<Long, Double[]>> A, int m) {
     MatrixWritable Tmm = new MatrixWritable();
     MatrixWritable Vm = new MatrixWritable();
 
-    Double[] a = new Double[m]; // alpha
-    Double[] b = new Double[m]; // beta
-    Vector[] v = new Vector[m];
-    Vector[] w = new Vector[m];
+    ArrayList<Double> a = new ArrayList<Double>(m);
+    ArrayList<Double> b = new ArrayList<Double>(m);
+    ArrayList<Vector> v = new ArrayList<Vector>(m);
+    ArrayList<Vector> w = new ArrayList<Vector>(m);
 
-    v[0] = new DenseVector(m).assign(0); // v[0] <-- 0
-    v[1] = new DenseVector(m).assign(1); // v[1] <-- Random vector with norm 1; TODO: Actually generate such a vector.
-    b[1] = 0.0;                          // b[1] <-- 0
+    v.set(0, new DenseVector(m).assign(0)); // v[0] <-- 0-vector
+    v.set(1, new DenseVector(m).assign(1)); // v[1] <-- Random vector with norm 1; TODO: Actually generate such a vector
+    b.set(1, 0.0);                          // b[1] <-- 0
 
     for (int j = 1; j < m; j++) {
       // TODO: w[j]   <-- A    * v[j]
