e1f780f457e76312341da486b4b57fa5640d9f45
Nik Hille <nik.hille@macio.de>
2015-01-28 19:04:33 +0100

    Using VectorElements for alpha and beta data sets (instead of Tuple2s)

--- src/de/tuberlin/dima/aim3/algorithms/Lanczos.java
+++ src/de/tuberlin/dima/aim3/algorithms/Lanczos.java
@@ -19,55 +19,61 @@ public final class Lanczos {
   public static void process(DataSet<Vector> A, int m) {
     ExecutionEnvironment env = A.getExecutionEnvironment();
 
-    ArrayList<Tuple2<Integer, Double>> aList = new ArrayList<Tuple2<Integer, Double>>();
-    ArrayList<Tuple2<Integer, Double>> bList = new ArrayList<Tuple2<Integer, Double>>();
-    ArrayList<Vector> vList = new ArrayList<Vector>();
-    ArrayList<Vector> wList = new ArrayList<Vector>();
+    ArrayList<VectorElement> aList = new ArrayList<VectorElement>();
+    ArrayList<VectorElement> bList = new ArrayList<VectorElement>();
+    ArrayList<Vector>        vList = new ArrayList<Vector>();
+    ArrayList<Vector>        wList = new ArrayList<Vector>();
 
-    vList.add(Vector.getZeroVector(3, 0));             // v[0] <-- 0-vector
-    vList.add(Vector.getRandomVector(3, 1, 1));        // v[1] <-- Random vector with norm 1
-    bList.add(0, new Tuple2<Integer, Double>(0, 0.0)); // b[0] <-- 0
-    bList.add(1, new Tuple2<Integer, Double>(1, 0.0)); // b[1] <-- 0
+    vList.add(Vector.getZeroVector(3, 0));      // v[0] <-- 0-vector
+    vList.add(Vector.getRandomVector(3, 1, 1)); // v[1] <-- Random vector with norm 1
+    bList.add(new VectorElement(0, 0.0));       // b[0] <-- 0.0
+    bList.add(new VectorElement(1, 0.0));       // b[1] <-- 0.0
 
     // Add an element to the a and w array lists, because you can't create DataSets from empty collections... These will
     // be filtered out by the RejectAll filter below.
-    aList.add(new Tuple2<Integer, Double>(0, 0.0));
+    aList.add(new VectorElement(0, 0.0));
     wList.add(Vector.getZeroVector(0));
 
     // Convert everything to data sets. For a and w, use the RejectAll filter to produce empty DataSets by filtering out
     // all elements.
-    DataSet<Tuple2<Integer, Double>> a = env.fromCollection(aList).filter(new RejectAll<Tuple2<Integer, Double>>());
-    DataSet<Tuple2<Integer, Double>> b = env.fromCollection(bList);
-    DataSet<Vector> v = env.fromCollection(vList);
-    DataSet<Vector> w = env.fromCollection(wList).filter(new RejectAll<Vector>());
+    DataSet<VectorElement> a = env.fromCollection(aList).filter(new RejectAll<VectorElement>());
+    DataSet<VectorElement> b = env.fromCollection(bList);
+    DataSet<Vector>        v = env.fromCollection(vList);
+    DataSet<Vector>        w = env.fromCollection(wList).filter(new RejectAll<Vector>());
 
-    // Within this loop, a, b, v and w have to be DataSets of some kind. Otherwise we're not able to properly process
-    // them with Flink. The problem is that in these DataSets, we have to preserve information about each element's
-    // position. For example, if a would be a DataSet<Double>, then we wouldn't know which iteration j a specific Double
-    // value in that set belongs to...
-    //
-    // --> Use Tuple2s containing the corresponding index for each element?
-    //     --> Not optimal; we still have to iterate over the tuples until we find the one we want.
     for (int j = 1; j < m; j++) {
       // Get vj by filtering out all v vectors with an index != j.
       DataSet<Vector> vj = v.filter(new VectorIndexFilter(j));
 
-      // TODO: w[j]   <-- A    * v[j]
+      // w[j] <-- A * v[j]
       DataSet<Vector> wj = A.groupBy("index").reduceGroup(new DotProduct())
                             .withBroadcastSet(vj, "otherVector")
                             .reduceGroup(new VectorElementsToSingleVector(j));
-      // TODO: Append wj to w!
+      // Don't append wj to w yet because it will be modified again later in this iteration.
 
-      vj.writeAsText(new File("data/out/v" + j + ".out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
-      wj.writeAsText(new File("data/out/w" + j + ".out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
+      // a[j] <-- w[j] * v[j]
+      DataSet<VectorElement> aj = wj.reduceGroup(new DotProduct())
+                                    .withBroadcastSet(vj, "otherVector");
+      a = a.union(aj);
 
-      // TODO: a[j]   <-- w[j] * v[j]
+
       // TODO: w[j]   <-- w[j] - a[j] * v[j] - b[j] * v[j-1]
+      w = w.union(wj);
+
       // TODO: b[j+1] <-- l2norm(w[j])
+
       // TODO: v[j+1] <-- w[j] / b[j+1]
 
       // TODO: If v[j+1] is not orthogonal to v[j] OR v[j+1] already exists in v, mark v[j+1] as "spurious".
+
+      // TODO: Remove test outputs!
+      vj.writeAsText(new File("data/out/v" + j + ".out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
+      wj.writeAsText(new File("data/out/w" + j + ".out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
+      aj.writeAsText(new File("data/out/a" + j + ".out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
     }
+
+    w.writeAsText(new File("data/out/w.out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
+    a.writeAsText(new File("data/out/a.out").getAbsolutePath(), FileSystem.WriteMode.OVERWRITE);
 
     // TODO: wm <-- A  * vm
     // TODO: am <-- wm * vm
