16ef4a6e53ab5509cd23fee152991b95f2b07fa7
Nik Hille <nhille030@gmail.com>
2015-01-30 18:00:18 +0100

    VectorScalarMultiplication operator: Fixed type of scalar

--- src/de/tuberlin/dima/aim3/operators/VectorScalarMultiplication.java
+++ src/de/tuberlin/dima/aim3/operators/VectorScalarMultiplication.java
@@ -1,6 +1,7 @@
 package de.tuberlin.dima.aim3.operators;
 
 import de.tuberlin.dima.aim3.datatypes.Vector;
+import de.tuberlin.dima.aim3.datatypes.VectorElement;
 import org.apache.flink.api.common.functions.RichGroupReduceFunction;
 import org.apache.flink.util.Collector;
 
@@ -19,7 +20,7 @@ public class VectorScalarMultiplication extends RichGroupReduceFunction<Vector, 
      */
     @Override
     public void reduce(Iterable<Vector> vectors, Collector<Vector> out) {
-        double scalar = getRuntimeContext().<Double>getBroadcastVariable("scalar").get(0);
+        double scalar = getRuntimeContext().<VectorElement>getBroadcastVariable("scalar").get(0).getValue();
         for (Vector vector : vectors) {
             out.collect(vector.times(scalar));
         }
